cc90c0b7884a65cb5aa2e333576cb1a8e304665e
Zachary <zbberman@yahoo.com>
2019-02-06 23:35:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,7 +111,6 @@ def player_numbers(team_name)
   game_hash.each do | team, data |
     if data[:team_name] == team_name
       data.each do | attribute, value |
-        binding.pry
         if attribute == :players
           numbers << value[:number]
           binding.pry
