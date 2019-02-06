fff043591ed2eb739b9b81bb8cd37ab57ab5bdf0
Zachary <zbberman@yahoo.com>
2019-02-06 22:47:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1 
-        }
+        },
         "Reggie Evans" => {
           :number => 30,
           :shoe => 14,
@@ -23,7 +23,7 @@ def game_hash
           :steals => 12,
           :blocks => 12,
           :slam_dunks => 7 
-        }
+        },
         "Brook Lopez" => {
           :number => 11,
           :shoe => 17,
@@ -33,7 +33,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 15 
-        }
+        },
         "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
@@ -43,7 +43,7 @@ def game_hash
           :steals => 3,
           :blocks => 8,
           :slam_dunks => 5
-        }
+        },
         "Jason Terry" => {
           :number => 31,
           :shoe => 15,
@@ -69,7 +69,7 @@ def game_hash
           :steals => 2,
           :blocks => 7,
           :slam_dunks => 2
-        }
+        },
         "Bismak Biyombo" => {
           :number => 0,
           :shoe => 16,
@@ -79,7 +79,7 @@ def game_hash
           :steals => 7,
           :blocks => 15,
           :slam_dunks => 10
-        }
+        },
         "DeSagna Diop" => {
           :number => 2,
           :shoe => 14,
@@ -89,7 +89,7 @@ def game_hash
           :steals => 4,
           :blocks => 5,
           :slam_dunks => 5
-        }
+        },
         "Ben Gordon" => {
           :number => 8,
           :shoe => 15,
@@ -99,7 +99,7 @@ def game_hash
           :steals => 1,
           :blocks => 1,
           :slam_dunks => 0
-        }
+        },
         "Brendan Haywood" => {
           :number => 33,
           :shoe => 15,
