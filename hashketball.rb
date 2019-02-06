4fdc80a7ec2d56c9ac853c4fd24148886b31401d
Zachary <zbberman@yahoo.com>
2019-02-06 22:49:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,31 +9,15 @@ def game_hash
           :steals => 3, :blocks => 1, :slam_dunks => 1 
         },
         "Reggie Evans" => {
-          :number => 30,
-          :shoe => 14,
-          :points => 12,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7 
+          :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12,
+          :steals => 12, :blocks => 12, :slam_dunks => 7 
         },
         "Brook Lopez" => {
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15 
+          :number => 11, :shoe => 17, :points => 17,  :rebounds => 19, :assists => 10,
+          :steals => 3, :blocks => 1, :slam_dunks => 15 
         },
         "Mason Plumlee" => {
-          :number => 1,
-          :shoe => 19,
-          :points => 26,
-          :rebounds => 12,
-          :assists => 6,
+          :number => 1, :shoe => 19, :points => 26, :rebounds => 12, :assists => 6,
           :steals => 3,
           :blocks => 8,
           :slam_dunks => 5
