4e084ade22461d616f5eaa6e231ac6e645d0464a
Zachary <zbberman@yahoo.com>
2019-02-06 22:50:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,18 +18,11 @@ def game_hash
         },
         "Mason Plumlee" => {
           :number => 1, :shoe => 19, :points => 26, :rebounds => 12, :assists => 6,
-          :steals => 3, :blocks => 8,
-          :slam_dunks => 5
+          :steals => 3, :blocks => 8, :slam_dunks => 5
         },
         "Jason Terry" => {
-          :number => 31,
-          :shoe => 15,
-          :points => 19,
-          :rebounds => 2,
-          :assists => 2,
-          :steals => 4,
-          :blocks => 11,
-          :slam_dunks => 1 
+          :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2,
+          :steals => 4, :blocks => 11, :slam_dunks => 1 
         }
       }
     }, 
@@ -38,24 +31,12 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => {
         "Jeff Adrien" => {
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
-          :slam_dunks => 2
+          :number => 4, :shoe => 18, :points => 10,  :rebounds => 1, :assists => 1,
+          :steals => 2, :blocks => 7, :slam_dunks => 2
         },
         "Bismak Biyombo" => {
-          :number => 0,
-          :shoe => 16,
-          :points => 12,
-          :rebounds => 4,
-          :assists => 7,
-          :steals => 7,
-          :blocks => 15,
-          :slam_dunks => 10
+          :number => 0, :shoe => 16,  :points => 12, :rebounds => 4, :assists => 7,
+          :steals => 7, :blocks => 15, :slam_dunks => 10
         },
         "DeSagna Diop" => {
           :number => 2,
