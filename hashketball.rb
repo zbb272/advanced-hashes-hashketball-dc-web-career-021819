bcaaacf266051c5ce32324cd5432848c8e8c533a
Zachary <zbberman@yahoo.com>
2019-02-06 22:48:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,14 +5,8 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1 
+          :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12,
+          :steals => 3, :blocks => 1, :slam_dunks => 1 
         },
         "Reggie Evans" => {
           :number => 30,
